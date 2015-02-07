2287bc30400de1ef563fb5e6af86b77d005762ee
Tim020 <timbradgate@hotmail.co.uk>
2015-02-07 21:09:02 +0000

    Added rendering handling for the filter chest:- ItemRenderer, TileEntitySpecialRenderer, ItemBlock and registered the TileEntity classes associated with this block

--- src/main/java/com/timthebrick/tinystorage/init/TileEntities.java
+++ src/main/java/com/timthebrick/tinystorage/init/TileEntities.java
@@ -1,10 +1,13 @@
 package com.timthebrick.tinystorage.init;
 
+import com.timthebrick.tinystorage.tileentity.TileEntityFilterChest;
+import com.timthebrick.tinystorage.tileentity.TileEntityFilterChestLarge;
+import com.timthebrick.tinystorage.tileentity.TileEntityFilterChestMedium;
+import com.timthebrick.tinystorage.tileentity.TileEntityFilterChestSmall;
 import com.timthebrick.tinystorage.tileentity.TileEntityTinyChest;
 import com.timthebrick.tinystorage.tileentity.TileEntityTinyChestLarge;
 import com.timthebrick.tinystorage.tileentity.TileEntityTinyChestMedium;
 import com.timthebrick.tinystorage.tileentity.TileEntityTinyChestSmall;
-import com.timthebrick.tinystorage.tileentity.TileEntityTinyStorage;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -15,6 +18,11 @@ public class TileEntities {
 		GameRegistry.registerTileEntityWithAlternatives(TileEntityTinyChestSmall.class, "blockTinyChestSmall", "tileEntityTinyChestSmall");
 		GameRegistry.registerTileEntityWithAlternatives(TileEntityTinyChestMedium.class, "blockTinyChestMedium", "tileEntityTinyChestMedium");
 		GameRegistry.registerTileEntityWithAlternatives(TileEntityTinyChestLarge.class, "blockTinyChestLarge", "tileEntityTinyChestLarge");
+		
+		GameRegistry.registerTileEntityWithAlternatives(TileEntityFilterChest.class, "blockFilterChest", "tileEntityFilterChest");
+		GameRegistry.registerTileEntityWithAlternatives(TileEntityFilterChestSmall.class, "blockFilterChestSmall", "tileEntityFilterChestSmall");
+		GameRegistry.registerTileEntityWithAlternatives(TileEntityFilterChestMedium.class, "blockFilterChestMedium", "tileEntityFilterChestMedium");
+		GameRegistry.registerTileEntityWithAlternatives(TileEntityFilterChestLarge.class, "blockFilterChestLarge", "tileEntityFilterChestLarge");
 	}
 
 }
---
+++ src/main/java/com/timthebrick/tinystorage/item/ItemBlockFilterChest.java
@@ -0,0 +1,38 @@
+package com.timthebrick.tinystorage.item;
+
+import java.util.List;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ItemBlockFilterChest extends ItemBlock {
+
+	public ItemBlockFilterChest(Block block) {
+		super(block);
+		this.setHasSubtypes(true);
+	}
+
+	@Override
+	public int getMetadata(int meta) {
+		return meta;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean flag) {
+		int metaData = itemStack.getItemDamage();
+
+		if (metaData == 0) {
+			list.add(StatCollector.translateToLocal("tooltip.tinystorage:filterChestPrefix.small"));
+		} else if (metaData == 1) {
+			list.add(StatCollector.translateToLocal("tooltip.tinystorage:filterChestPrefix.medium"));
+		} else if (metaData == 2) {
+			list.add(StatCollector.translateToLocal("tooltip.tinystorage:filterChestPrefix.large"));
+		}
+	}
+
+}
