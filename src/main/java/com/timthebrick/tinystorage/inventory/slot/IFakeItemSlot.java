912e2a75cda43c02d0f1e2f8165a7ac7ae039fca
Tim020 <timbradgate@hotmail.co.uk>
2015-02-09 20:51:04 +0000

    Added fake item stacks

---
+++ src/main/java/com/timthebrick/tinystorage/inventory/slot/IFakeItemSlot.java
@@ -0,0 +1,7 @@
+package com.timthebrick.tinystorage.inventory.slot;
+
+public interface IFakeItemSlot {
+	
+	boolean canAdjust();
+
+}
--- src/main/java/com/timthebrick/tinystorage/inventory/slot/SlotTinyStorage.java
+++ src/main/java/com/timthebrick/tinystorage/inventory/slot/SlotTinyStorage.java
@@ -1,31 +1,13 @@
 package com.timthebrick.tinystorage.inventory.slot;
 
+import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
 public class SlotTinyStorage extends Slot {
 
-	protected ItemStack filterStack;
-
-	public SlotTinyStorage(IInventory inventory, int id, int xPos, int yPos, ItemStack filterStack) {
-		super(inventory, id, xPos, yPos);
-		this.filterStack = filterStack;
+	public SlotTinyStorage(IInventory inv, int id, int x, int y) {
+		super(inv, id, x, y);
 	}
-
-	@Override
-	public boolean isItemValid(ItemStack stack) {
-		if (filterStack != null) {
-			if (stack.getItem() == filterStack.getItem()) {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
-	}
-	
-	
-
 }
