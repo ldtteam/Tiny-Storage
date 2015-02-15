9e9660f238ed64f8431a49ca1916f3e82d755c61
Tim020 <timbradgate@hotmail.co.uk>
2015-02-15 10:29:49 +0000

    Added a Crafting Handler and Recipes class ready for additions of crafting recipes - at long last!

---
+++ src/main/java/com/timthebrick/tinystorage/handler/CraftingHandler.java
@@ -0,0 +1,17 @@
+package com.timthebrick.tinystorage.handler;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+
+public class CraftingHandler {
+	
+	public static void init(){
+		
+	}
+	
+	@SubscribeEvent
+	public void onItemCraftedEvent(PlayerEvent.ItemCraftedEvent event){
+		
+	}
+
+}
---
+++ src/main/java/com/timthebrick/tinystorage/init/Recipes.java
@@ -0,0 +1,12 @@
+package com.timthebrick.tinystorage.init;
+
+import com.timthebrick.tinystorage.core.TinyStorageLog;
+
+public class Recipes {
+	
+	public static void init(){
+		TinyStorageLog.info("Initialising Crafting Recipes");
+		
+	}
+
+}
