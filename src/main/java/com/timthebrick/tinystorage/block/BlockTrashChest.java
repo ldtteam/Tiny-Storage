f74af2cebb964787695867f0ade6d386f799e38c
Tim020 <timbradgate@hotmail.co.uk>
2015-02-19 18:09:18 +0000

    Finalizing work on the trash chest

--- src/main/java/com/timthebrick/tinystorage/block/BlockTrashChest.java
+++ src/main/java/com/timthebrick/tinystorage/block/BlockTrashChest.java
@@ -1,35 +1,72 @@
 package com.timthebrick.tinystorage.block;
 
+import java.util.Random;
+
+import com.timthebrick.tinystorage.TinyStorage;
 import com.timthebrick.tinystorage.creativetab.TabTinyStorage;
+import com.timthebrick.tinystorage.reference.GUIs;
+import com.timthebrick.tinystorage.reference.References;
 import com.timthebrick.tinystorage.reference.RenderIDs;
+import com.timthebrick.tinystorage.tileentity.TileEntityTinyChest;
 import com.timthebrick.tinystorage.tileentity.TileEntityTinyStorage;
 import com.timthebrick.tinystorage.tileentity.TileEntityTrashChest;
+import com.timthebrick.tinystorage.util.PlayerHelper;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MathHelper;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
 public class BlockTrashChest extends BlockContainer implements ITileEntityProvider {
 
-	protected String textureName;
-
-	public BlockTrashChest(Material mat, String textureName) {
+	public BlockTrashChest(Material mat) {
 		super(mat);
-		this.setHardness(2.5f);
-		this.textureName = textureName;
-		this.setBlockName("blockTrashChest" + this.textureName);
+		this.setHardness(50.0F);
+		this.setResistance(2000.0F);
+		this.setBlockName("blockTrashChest");
 		this.setCreativeTab(TabTinyStorage.creativeTab);
 	}
 
 	@Override
 	public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_) {
 		return new TileEntityTrashChest();
+	}
+
+	@Override
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z) {
+		setBlockBoundsBasedOnState(world, x, y, z);
+		return super.getCollisionBoundingBoxFromPool(world, x, y, z);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public AxisAlignedBB getSelectedBoundingBoxFromPool(World world, int x, int y, int z) {
+		setBlockBoundsBasedOnState(world, x, y, z);
+		return super.getSelectedBoundingBoxFromPool(world, x, y, z);
+	}
+
+	@Override
+	public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z) {
+		updateChestBounds(world.getBlockMetadata(x, y, z));
+	}
+
+	public void updateChestBounds(int meta) {
+		setBlockBounds(0.2f, 0.0f, 0.2f, 0.8f, 0.51f, 0.8f);
 	}
 
 	@Override
@@ -46,12 +83,30 @@ public class BlockTrashChest extends BlockContainer implements ITileEntityProvid
 	public int getRenderType() {
 		return RenderIDs.trashChest;
 	}
-	
+
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int par6, float par7, float par8, float par9) {
+		if ((player.isSneaking() && player.getCurrentEquippedItem() != null) || world.isSideSolid(x, y + 1, z, ForgeDirection.DOWN)) {
+			return true;
+		} else {
+			if (!world.isRemote && world.getTileEntity(x, y, z) instanceof TileEntityTrashChest) {
+				player.openGui(TinyStorage.instance, GUIs.TRASH_CHEST.ordinal(), world, x, y, z);
+			}
+			return true;
+		}
+	}
+
 	@Override
 	public boolean onBlockEventReceived(World world, int x, int y, int z, int eventId, int eventData) {
 		super.onBlockEventReceived(world, x, y, z, eventId, eventData);
 		TileEntity tileentity = world.getTileEntity(x, y, z);
 		return tileentity != null && tileentity.receiveClientEvent(eventId, eventData);
+	}
+
+	@Override
+	public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
+		dropInventory(world, x, y, z);
+		super.breakBlock(world, x, y, z, block, meta);
 	}
 
 	@Override
@@ -77,9 +132,44 @@ public class BlockTrashChest extends BlockContainer implements ITileEntityProvid
 			((TileEntityTinyStorage) world.getTileEntity(x, y, z)).setOrientation(direction);
 		}
 	}
-	
+
+	protected void dropInventory(World world, int x, int y, int z) {
+		TileEntity tileEntity = world.getTileEntity(x, y, z);
+
+		if (!(tileEntity instanceof IInventory)) {
+			return;
+		}
+
+		IInventory inventory = (IInventory) tileEntity;
+
+		for (int i = 0; i < inventory.getSizeInventory() - 1; i++) {
+			ItemStack itemStack = inventory.getStackInSlot(i);
+
+			if (itemStack != null && itemStack.stackSize > 0) {
+				Random rand = new Random();
+
+				float dX = rand.nextFloat() * 0.8F + 0.1F;
+				float dY = rand.nextFloat() * 0.8F + 0.1F;
+				float dZ = rand.nextFloat() * 0.8F + 0.1F;
+
+				EntityItem entityItem = new EntityItem(world, x + dX, y + dY, z + dZ, itemStack.copy());
+
+				if (itemStack.hasTagCompound()) {
+					entityItem.getEntityItem().setTagCompound((NBTTagCompound) itemStack.getTagCompound().copy());
+				}
+
+				float factor = 0.05F;
+				entityItem.motionX = rand.nextGaussian() * factor;
+				entityItem.motionY = rand.nextGaussian() * factor + 0.2F;
+				entityItem.motionZ = rand.nextGaussian() * factor;
+				world.spawnEntityInWorld(entityItem);
+				itemStack.stackSize = 0;
+			}
+		}
+	}
+
 	@Override
-	public String getTextureName() {
-		return textureName;
+	public void registerBlockIcons(IIconRegister iconRegister) {
+		blockIcon = iconRegister.registerIcon(References.MOD_ID.toLowerCase() + ":blockTinyChest");
 	}
 }
---
+++ src/main/java/com/timthebrick/tinystorage/item/block/ItemBlockTrashChest.java
@@ -0,0 +1,12 @@
+package com.timthebrick.tinystorage.item.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+
+public class ItemBlockTrashChest extends ItemBlock {
+
+	public ItemBlockTrashChest(Block block) {
+		super(block);
+	}
+
+}
