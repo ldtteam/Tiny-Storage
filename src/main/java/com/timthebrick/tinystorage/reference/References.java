76673574fd1f294c0e062a2d9913f1399aecac52
Tim020 <timbradgate@hotmail.co.uk>
2015-05-12 22:50:00 +0100

    Updated the files needed for dev release

--- src/main/java/com/timthebrick/tinystorage/reference/References.java
+++ src/main/java/com/timthebrick/tinystorage/reference/References.java
@@ -4,7 +4,7 @@ public class References {
 	
 	public static final String MOD_ID = "TinyStorage";
 	public static final String MOD_NAME = "Tiny Storage";
-	public static final String VERSION = "1.7.10-1.10.1";
+	public static final String VERSION = "1.7.10-1.12.0-Dev-1";
 	public static final String SERVER_PROXY_CLASS = "com.timthebrick.tinystorage.proxy.ServerProxy";
 	public static final String CLIENT_PROXY_CLASS = "com.timthebrick.tinystorage.proxy.ClientProxy";
 	public static final String GUI_FACTORY_CLASS = "com.timthebrick.tinystorage.client.gui.GuiFactory";
