49a947cb28ef637bfa59059ad0f840d0da32866e
Tim020 <timbradgate@hotmail.co.uk>
2015-05-31 17:43:54 +0100

    Added to the circle helper to give methods that return an ArrayList of all blocks within that circle

--- src/main/java/com/timthebrick/tinystorage/util/CircleHelper.java
+++ src/main/java/com/timthebrick/tinystorage/util/CircleHelper.java
@@ -1,107 +1,165 @@
 package com.timthebrick.tinystorage.util;
 
+import java.util.ArrayList;
+
+import net.minecraft.block.Block;
 import net.minecraft.world.World;
 
 import com.timthebrick.tinystorage.core.TinyStorageLog;
 
 /**
- * Created by Orion
- * Created on 31.05.2015
- * 13:32
+ * Created by Orion Created on 31.05.2015 13:32
  * <p/>
  * Copyrighted according to Project specific license
  */
 public class CircleHelper {
 
-	public static void FillCircle(int pOriginX, int pOriginY, int pOriginZ, World pWorldObj, int pRadius) {
-		pWorldObj.setBlockToAir(pOriginX, pOriginY, pOriginZ);
-
-		for (int tStep = 1; tStep <= pRadius; tStep++) {
-			SetCircleIncNeigborCheck(pOriginX, pOriginY, pOriginZ, pWorldObj, tStep);
+	public static void fillCircle(int originX, int originY, int originZ, World world, int radius) {
+		world.setBlockToAir(originX, originY, originZ);
+		for (int tStep = 1; tStep <= radius; tStep++) {
+			setCircleIncNeigborCheck(originX, originY, originZ, world, tStep);
 		}
 	}
+	
+	public static ArrayList genCircle(int originX, int originY, int originZ, World world, int radius){
+		ArrayList blocks = new ArrayList<Block>();
+		blocks.add(world.getBlock(originX, originY, originZ));
+		for (int tStep = 1; tStep <= radius; tStep++) {
+			getCircleIncNeigborCheck(originX, originY, originZ, world, tStep, blocks);
+		}
+		return blocks;
+	}
 
-	public static void SetCircle(int pOriginX, int pOriginY, int pOriginZ, World pWorldObj, int pRadius) {
-		int tCurrentX = pRadius;
-		int tCurrentZ = 0;
-		int decisionOver2 = 1 - tCurrentX;
+	public static void setCircle(int originX, int originY, int originZ, World world, int pRadius) {
+		int currentX = pRadius;
+		int currentZ = 0;
+		int decisionOver2 = 1 - currentX;
 
-		while (tCurrentX >= tCurrentZ) {
-			pWorldObj.setBlockToAir(tCurrentX + pOriginX, pOriginY, tCurrentZ + pOriginZ);
-			pWorldObj.setBlockToAir(tCurrentZ + pOriginX, pOriginY, tCurrentX + pOriginZ);
-			pWorldObj.setBlockToAir(-tCurrentX + pOriginX, pOriginY, tCurrentZ + pOriginZ);
-			pWorldObj.setBlockToAir(-tCurrentZ + pOriginX, pOriginY, tCurrentX + pOriginZ);
-			pWorldObj.setBlockToAir(-tCurrentX + pOriginX, pOriginY, -tCurrentZ + pOriginZ);
-			pWorldObj.setBlockToAir(-tCurrentZ + pOriginX, pOriginY, -tCurrentX + pOriginZ);
-			pWorldObj.setBlockToAir(tCurrentX + pOriginX, pOriginY, -tCurrentZ + pOriginZ);
-			pWorldObj.setBlockToAir(tCurrentZ + pOriginX, pOriginY, -tCurrentX + pOriginZ);
-			tCurrentZ++;
+		while (currentX >= currentZ) {
+			world.setBlockToAir(currentX + originX, originY, currentZ + originZ);
+			world.setBlockToAir(currentZ + originX, originY, currentX + originZ);
+			world.setBlockToAir(-currentX + originX, originY, currentZ + originZ);
+			world.setBlockToAir(-currentZ + originX, originY, currentX + originZ);
+			world.setBlockToAir(-currentX + originX, originY, -currentZ + originZ);
+			world.setBlockToAir(-currentZ + originX, originY, -currentX + originZ);
+			world.setBlockToAir(currentX + originX, originY, -currentZ + originZ);
+			world.setBlockToAir(currentZ + originX, originY, -currentX + originZ);
+			currentZ++;
 
 			if (decisionOver2 <= 0) {
-				decisionOver2 += 2 * tCurrentZ + 1;
+				decisionOver2 += 2 * currentZ + 1;
 
 			} else {
-				tCurrentX--;
-				decisionOver2 += 2 * (tCurrentZ - tCurrentX) + 1;
+				currentX--;
+				decisionOver2 += 2 * (currentZ - currentX) + 1;
 			}
 		}
 	}
+	
+	public static void getCircleIncNeigborCheck(int originX, int originY, int originZ, World world, int pRadius, ArrayList<Block> blocks) {
+		int currentX = pRadius;
+		int currentZ = 0;
+		int decisionOver2 = 1 - currentX;
 
-	public static void SetCircleIncNeigborCheck(int pOriginX, int pOriginY, int pOriginZ, World pWorldObj, int pRadius) {
-		int tCurrentX = pRadius;
-		int tCurrentZ = 0;
-		int decisionOver2 = 1 - tCurrentX; // Decision criterion divided by 2
-											// evaluated at x=r, y=0
-
-		while (tCurrentX >= tCurrentZ) {
-			SetBlockIncNeighborCheck(tCurrentX + pOriginX, pOriginY, tCurrentZ + pOriginZ, tCurrentX, 0, tCurrentZ, pWorldObj);
-			SetBlockIncNeighborCheck(tCurrentZ + pOriginX, pOriginY, tCurrentX + pOriginZ, tCurrentZ, 0, tCurrentX, pWorldObj);
-			SetBlockIncNeighborCheck(-tCurrentX + pOriginX, pOriginY, tCurrentZ + pOriginZ, -tCurrentX, 0, tCurrentZ, pWorldObj);
-			SetBlockIncNeighborCheck(-tCurrentZ + pOriginX, pOriginY, tCurrentX + pOriginZ, -tCurrentZ, 0, tCurrentX, pWorldObj);
-			SetBlockIncNeighborCheck(-tCurrentX + pOriginX, pOriginY, -tCurrentZ + pOriginZ, -tCurrentX, 0, -tCurrentZ, pWorldObj);
-			SetBlockIncNeighborCheck(-tCurrentZ + pOriginX, pOriginY, -tCurrentX + pOriginZ, -tCurrentZ, 0, -tCurrentX, pWorldObj);
-			SetBlockIncNeighborCheck(tCurrentX + pOriginX, pOriginY, -tCurrentZ + pOriginZ, tCurrentX, 0, -tCurrentZ, pWorldObj);
-			SetBlockIncNeighborCheck(tCurrentZ + pOriginX, pOriginY, -tCurrentX + pOriginZ, tCurrentZ, 0, -tCurrentX, pWorldObj);
-			tCurrentZ++;
+		while (currentX >= currentZ) {
+			getBlockIncNeighborCheck(currentX + originX, originY, currentZ + originZ, currentX, 0, currentZ, world, blocks);
+			getBlockIncNeighborCheck(currentZ + originX, originY, currentX + originZ, currentZ, 0, currentX, world, blocks);
+			getBlockIncNeighborCheck(-currentX + originX, originY, currentZ + originZ, -currentX, 0, currentZ, world, blocks);
+			getBlockIncNeighborCheck(-currentZ + originX, originY, currentX + originZ, -currentZ, 0, currentX, world, blocks);
+			getBlockIncNeighborCheck(-currentX + originX, originY, -currentZ + originZ, -currentX, 0, -currentZ, world, blocks);
+			getBlockIncNeighborCheck(-currentZ + originX, originY, -currentX + originZ, -currentZ, 0, -currentX, world, blocks);
+			getBlockIncNeighborCheck(currentX + originX, originY, -currentZ + originZ, currentX, 0, -currentZ, world, blocks);
+			getBlockIncNeighborCheck(currentZ + originX, originY, -currentX + originZ, currentZ, 0, -currentX, world, blocks);
+			currentZ++;
 
 			if (decisionOver2 <= 0) {
-				decisionOver2 += 2 * tCurrentZ + 1; // Change in decision
-													// criterion for y -> y+1
+				decisionOver2 += 2 * currentZ + 1;
 			} else {
-				tCurrentX--;
-				decisionOver2 += 2 * (tCurrentZ - tCurrentX) + 1; // Change for
-																	// y -> y+1,
-																	// x -> x-1
+				currentX--;
+				decisionOver2 += 2 * (currentZ - currentX) + 1;
 			}
 		}
 	}
-
-	private static void SetBlockIncNeighborCheck(int pOriginX, int pOriginY, int pOriginZ, int pRelativeX, int pRelativeY, int pRelativeZ, World pWorldObj) {
-		pWorldObj.setBlockToAir(pOriginX, pOriginY, pOriginZ);
+	
+	private static void getBlockIncNeighborCheck(int originX, int originY, int originZ, int relativeX, int relativeY, int relativeZ, World world, ArrayList<Block> blocks) {
+		world.setBlockToAir(originX, originY, originZ);
 
 		int tRelativeX = 0;
 		int tRelativeZ = 0;
 
-		if (pRelativeX > 0) {
+		if (relativeX > 0) {
 			tRelativeX = -1;
 		}
 
-		if (pRelativeX < 0) {
+		if (relativeX < 0) {
 			tRelativeX = 1;
 		}
 
-		if (pRelativeZ > 0) {
+		if (relativeZ > 0) {
 			tRelativeZ = -1;
 		}
 
-		if (pRelativeZ < 0) {
+		if (relativeZ < 0) {
 			tRelativeZ = 1;
 		}
 
-		TinyStorageLog.info(pRelativeX + "-" + pRelativeY + "-" + pRelativeZ + "/" + tRelativeX + "-" + tRelativeZ);
-		pWorldObj.setBlockToAir(pOriginX + tRelativeX, pOriginY, pOriginZ + tRelativeZ);
-		pWorldObj.setBlockToAir(pOriginX + tRelativeX, pOriginY, pOriginZ);
-		pWorldObj.setBlockToAir(pOriginX, pOriginY, pOriginZ + tRelativeZ);
+		TinyStorageLog.info(relativeX + "-" + relativeY + "-" + relativeZ + "/" + tRelativeX + "-" + tRelativeZ);
+		blocks.add(world.getBlock(originX + tRelativeX, originY, originZ + tRelativeZ));
+		blocks.add(world.getBlock(originX + tRelativeX, originY, originZ));
+		blocks.add(world.getBlock(originX, originY, originZ + tRelativeZ));
+	}
+
+	public static void setCircleIncNeigborCheck(int originX, int originY, int originZ, World world, int pRadius) {
+		int currentX = pRadius;
+		int currentZ = 0;
+		int decisionOver2 = 1 - currentX;
+
+		while (currentX >= currentZ) {
+			setBlockIncNeighborCheck(currentX + originX, originY, currentZ + originZ, currentX, 0, currentZ, world);
+			setBlockIncNeighborCheck(currentZ + originX, originY, currentX + originZ, currentZ, 0, currentX, world);
+			setBlockIncNeighborCheck(-currentX + originX, originY, currentZ + originZ, -currentX, 0, currentZ, world);
+			setBlockIncNeighborCheck(-currentZ + originX, originY, currentX + originZ, -currentZ, 0, currentX, world);
+			setBlockIncNeighborCheck(-currentX + originX, originY, -currentZ + originZ, -currentX, 0, -currentZ, world);
+			setBlockIncNeighborCheck(-currentZ + originX, originY, -currentX + originZ, -currentZ, 0, -currentX, world);
+			setBlockIncNeighborCheck(currentX + originX, originY, -currentZ + originZ, currentX, 0, -currentZ, world);
+			setBlockIncNeighborCheck(currentZ + originX, originY, -currentX + originZ, currentZ, 0, -currentX, world);
+			currentZ++;
+
+			if (decisionOver2 <= 0) {
+				decisionOver2 += 2 * currentZ + 1;
+			} else {
+				currentX--;
+				decisionOver2 += 2 * (currentZ - currentX) + 1;
+			}
+		}
+	}
+
+	private static void setBlockIncNeighborCheck(int originX, int originY, int originZ, int relativeX, int relativeY, int relativeZ, World world) {
+		world.setBlockToAir(originX, originY, originZ);
+
+		int tRelativeX = 0;
+		int tRelativeZ = 0;
+
+		if (relativeX > 0) {
+			tRelativeX = -1;
+		}
+
+		if (relativeX < 0) {
+			tRelativeX = 1;
+		}
+
+		if (relativeZ > 0) {
+			tRelativeZ = -1;
+		}
+
+		if (relativeZ < 0) {
+			tRelativeZ = 1;
+		}
+
+		TinyStorageLog.info(relativeX + "-" + relativeY + "-" + relativeZ + "/" + tRelativeX + "-" + tRelativeZ);
+		world.setBlockToAir(originX + tRelativeX, originY, originZ + tRelativeZ);
+		world.setBlockToAir(originX + tRelativeX, originY, originZ);
+		world.setBlockToAir(originX, originY, originZ + tRelativeZ);
 	}
 
 }
