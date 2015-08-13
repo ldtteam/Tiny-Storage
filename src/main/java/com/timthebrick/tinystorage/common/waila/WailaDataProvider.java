788c7ec2c754f9577039e3e6827cc2920754a374
Tim020 <timbradgate@hotmail.co.uk>
2015-08-13 20:16:05 +0100

    Added a Waila tooltip to show what IO mode a locked storage option is in; note this only shows to player who's object it is

--- src/main/java/com/timthebrick/tinystorage/common/waila/WailaDataProvider.java
+++ src/main/java/com/timthebrick/tinystorage/common/waila/WailaDataProvider.java
@@ -1,5 +1,11 @@
 package com.timthebrick.tinystorage.common.waila;
 
+import com.timthebrick.tinystorage.TinyStorage;
+import com.timthebrick.tinystorage.client.gui.widgets.settings.AccessMode;
+import com.timthebrick.tinystorage.common.core.TinyStorageLog;
+import com.timthebrick.tinystorage.common.reference.Messages;
+import com.timthebrick.tinystorage.common.tileentity.TileEntityTinyStorage;
+import com.timthebrick.tinystorage.common.tileentity.implementations.TileEntityTinyChest;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
@@ -8,6 +14,7 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -26,6 +33,20 @@ public class WailaDataProvider implements IWailaDataProvider {
 
     @Override
     public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        if (accessor.getTileEntity() instanceof TileEntityTinyStorage) {
+            TileEntityTinyStorage tileEntity = (TileEntityTinyStorage) accessor.getTileEntity();
+            if (tileEntity.hasUniqueOwner() && tileEntity.getUniqueOwner().equals(accessor.getPlayer().getUniqueID().toString() + accessor.getPlayer().getDisplayName())) {
+                if (tileEntity.getAccessMode() == AccessMode.DISABLED) {
+                    currenttip.add(0, StatCollector.translateToLocal(Messages.ButtonTooltips.ACCESS_MODE_TITLE) + ": " + StatCollector.translateToLocal(Messages.ButtonTooltips.ACCESS_MODE_BLOCKED));
+                } else if (tileEntity.getAccessMode() == AccessMode.INPUT_ONLY) {
+                    currenttip.add(0, StatCollector.translateToLocal(Messages.ButtonTooltips.ACCESS_MODE_TITLE) + ": " + StatCollector.translateToLocal(Messages.ButtonTooltips.ACCESS_MODE_INPUT_ONLY));
+                } else if (tileEntity.getAccessMode() == AccessMode.OUTPUT_ONLY) {
+                    currenttip.add(0, StatCollector.translateToLocal(Messages.ButtonTooltips.ACCESS_MODE_TITLE) + ": " + StatCollector.translateToLocal(Messages.ButtonTooltips.ACCESS_MODE_OUTPUT_ONLY));
+                } else if (tileEntity.getAccessMode() == AccessMode.INPUT_OUTPUT) {
+                    currenttip.add(0, StatCollector.translateToLocal(Messages.ButtonTooltips.ACCESS_MODE_TITLE) + ": " + StatCollector.translateToLocal(Messages.ButtonTooltips.ACCESS_MODE_BOTH));
+                }
+            }
+        }
         return currenttip;
     }
 
@@ -40,7 +61,8 @@ public class WailaDataProvider implements IWailaDataProvider {
     }
 
     public static void callbackRegister(IWailaRegistrar registrar) {
-
+        TinyStorageLog.info("Registering Waila Data Providers");
+        registrar.registerBodyProvider(new WailaDataProvider(), TileEntityTinyStorage.class);
     }
 
 }
