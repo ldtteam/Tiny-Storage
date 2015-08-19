b3cca87bc144d81cf7e1fbd99f3fa79682c246d5
Tim020 <timbradgate@hotmail.co.uk>
2015-08-19 19:19:04 +0100

    Reset to the last commit

---
+++ src/main/java/com/timthebrick/tinystorage/util/client/colour/BlockPlaceHolderRegistrar.java
@@ -0,0 +1,13 @@
+package com.timthebrick.tinystorage.util.client.colour;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.ResourceLocation;
+
+public class BlockPlaceHolderRegistrar implements IIconRegister {
+
+    @Override
+    public IIcon registerIcon(String pTextureLocation) {
+        return new BlockIconPlaceHolder(new ResourceLocation( pTextureLocation));
+    }
+}
