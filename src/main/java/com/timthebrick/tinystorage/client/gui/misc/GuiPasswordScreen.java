25a924dde8e3876e0c37db27cb5070b3e164d9f2
Tim020 <timbradgate@hotmail.co.uk>
2015-09-26 10:12:10 +0100

    Oops, package name the same as a folder name not getting synced... That was interesting

---
+++ src/main/java/com/timthebrick/tinystorage/client/gui/misc/GuiPasswordScreen.java
@@ -0,0 +1,6 @@
+package com.timthebrick.tinystorage.client.gui.misc;
+
+import com.timthebrick.tinystorage.client.gui.GuiTinyStorageSimple;
+
+public class GuiPasswordScreen extends GuiTinyStorageSimple {
+}
