e6c055103ec3b8bbeb39719b8f203b6d3f3e945d
Tim020 <timbradgate@hotmail.co.uk>
2016-06-19 22:29:22 +0100

    Added basic mod files, it runs.... It's a start

---
+++ src/main/java/com/smithsmodding/tinystorage/common/proxy/IProxy.java
@@ -0,0 +1,26 @@
+package com.smithsmodding.tinystorage.common.proxy;
+
+/**
+ * Created by Tim on 19/06/2016.
+ */
+public interface IProxy {
+
+    ClientProxy getClientProxy ();
+
+    CommonProxy getCommonProxy();
+
+    void initRenderingAndTextures ();
+
+    void registerEventHandlers ();
+
+    void registerKeyBindings ();
+
+    String getMinecraftVersion ();
+
+    void preInit();
+
+    void init();
+
+    void postInit();
+
+}
