f813c1655f1dc180ea2d68b0ed0307f89981d6f4
Tim020 <timbradgate@hotmail.co.uk>
2016-06-22 17:26:11 +0100

    Storage module interface

---
+++ src/main/java/com/smithsmodding/tinystorage/api/common/modules/IStorageModule.java
@@ -0,0 +1,9 @@
+package com.smithsmodding.tinystorage.api.common.modules;
+
+import com.smithsmodding.smithscore.common.inventory.IItemStorage;
+
+/**
+ * Created by Tim on 22/06/2016.
+ */
+public interface IStorageModule extends IModule, IItemStorage {
+}
