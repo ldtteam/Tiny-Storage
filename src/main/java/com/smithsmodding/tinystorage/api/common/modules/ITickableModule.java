d6c521a6997d92cf582b08efa7c3e2fc12a8d198
Orion <oriondevelopment@outlook.com>
2016-06-22 18:36:35 +0200

    Added a Tickable module

---
+++ src/main/java/com/smithsmodding/tinystorage/api/common/modules/ITickableModule.java
@@ -0,0 +1,9 @@
+package com.smithsmodding.tinystorage.api.common.modules;
+
+import net.minecraft.util.ITickable;
+
+/**
+ * Author Orion (Created on: 22.06.2016)
+ */
+public interface ITickableModule extends ITickable, IModule {
+}
